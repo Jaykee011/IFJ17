6766d1b116a164c14f8708f338e06ad948f52462
Jakub Zapletal <jacob.zpl@gmail.com>
2017-10-29 12:00:25 +0100

    Revert "Merge branch 'master' of https://github.com/Jaykee011/IFJ17"
    This reverts commit a322f6757788df4f60c5f5e14ccefbd9c9a656c2, reversing
    changes made to 962a9f78ae8e59a6e08b7c557d797f81fddf3e8d.

---
+++ Source/error.h
@@ -0,0 +1,17 @@
+/*
+ * IFJ 2017 project
+ *	File			error.h
+ *	Description		Header file for error.c
+ *	Author			Michal Zilvar (xzilva02)
+ *	Last update		11:24, 29-10-2017
+ */
+
+#ifndef ERRORH
+#define ERRORH
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include "macros.h"
+
+#endif
--- Source/includes.h
+++ Source/includes.h
@@ -10,6 +10,7 @@
 #include <string.h>
 #include "define.h"
 #include "macros.h"
+#include "error.h"
 //#include "symtable.h"
 #include "string.h"
 #include "scanner.h"
