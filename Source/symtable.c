12aeb139d61734d6d81d6a29d954427afa698ef7
Studna555 <xstude23@stud.fit.vutbr.cz>
2017-11-19 16:02:44 +0100

    Add files via upload

--- Source/symtable.c
+++ Source/symtable.c
@@ -4,7 +4,25 @@
  *	Description		Source file for binary search tree
  *	Author			Martin Studeny (xstude23)
  */
-#include "includes.h"
+#include "symtable.h"
+
+extern nodePtr Strom;
+
+struct load insert;
+loadPtr Content_of_Insert = &insert;
+
+int stringCpy(String *s, char* c) {
+	if(s == NULL)
+		if(stringInit(&s))
+			return INTERN_ERR;
+
+	for(int i = 0; c[i] != '\0'; i++) {
+		if(stringAddData(s, c[i]))
+			return INTERN_ERR;
+	}
+
+	return FINE; // TODO: magie
+}
 
 void treeInit(nodePtr *tree) {
     *tree = NULL;
@@ -14,42 +32,40 @@ void treeDispose(nodePtr *tree) {
 	if( (*tree) != NULL) {
 		treeDispose(&(*tree)->lPtr);
 		treeDispose(&(*tree)->rPtr);
-		free(*tree);
 	}
 	*tree = NULL;
 }
 
-nodePtr nodeSearch(nodePtr tree, char* K, loadPtr *content) {
+nodePtr nodeSearch(nodePtr tree, char* K) {
 	if(tree != NULL) {
-		if(tree->key == K) {
-			*content = tree->symbol;
+		if(strcmp(tree->key, K) == 0) {
+			//*content = tree->symbol;
 			return tree;
 		}
-		else if(tree->key > K)
-			return nodeSearch(tree->lPtr, K,content);
+		else if(strcmp(tree->key, K) > 0)
+			return nodeSearch(tree->lPtr, K/*,content*/);
 		else
-			return nodeSearch(tree->rPtr, K,content);
+			return nodeSearch(tree->rPtr, K/*,content*/);
 	}
 	else
 		return NULL;
 }
 
-nodePtr nodeInsert(nodePtr* tree, loadPtr load, char* K) {
+void nodeInsert(nodePtr* tree, loadPtr load, char* K) {
 	if(*tree == NULL) {
-		*tree = malloc(sizeof(struct node));
-		(*tree)->key = K;
+		*tree = saveMalloc(sizeof(struct node));
+		strcpy((*tree)->key, K);
 		(*tree)->lPtr = NULL;
 		(*tree)->rPtr = NULL;
 		(*tree)->symbol = load;
-		return;
 	}
 	else {
-		if((*tree)->key == K) {
+		if(strcmp((*tree)->key, K) == 0) {
 			(*tree)->symbol = load;
 			return;
 		}
 		else {
-			if((*tree)->key > K)
+			if(strcmp((*tree)->key, K) > 0)
 				nodeInsert(&(*tree)->lPtr, load, K);
 			else
 				nodeInsert(&(*tree)->rPtr, load, K);
@@ -58,51 +74,40 @@ nodePtr nodeInsert(nodePtr* tree, loadPtr load, char* K) {
 }
 
 void ReplaceByRightmost(nodePtr treeReplaced, nodePtr *tree) {
-	nodePtr tmp = NULL;
 	if((*tree)->rPtr != NULL) {
 		ReplaceByRightmost(treeReplaced, &(*tree)->rPtr);
 		return;
 	}
 	else {
 		treeReplaced->symbol = (*tree)->symbol;
-		treeReplaced->key = (*tree)->key;
-		tmp = *tree;
+		strcpy(treeReplaced->key,(*tree)->key);
 		*tree = (*tree)->lPtr;
-		free(tmp);
 	}
 }
 
 void nodeDelete(nodePtr *tree, char *K) {
-	static nodePtr tmp = NULL;
 	if(*tree == NULL)
 		return;
-	if((*tree)->key == K) {
+	if(strcmp((*tree)->key, K) == 0) {
 		if((*tree)->lPtr != NULL && (*tree)->rPtr != NULL) {
 			ReplaceByRightmost(*tree, &(*tree)->lPtr);
 			return;
 		}
 		if((*tree)->lPtr != NULL && (*tree)->rPtr == NULL) {
-			tmp = *tree;
 			*tree = (*tree)->lPtr;
-			free(tmp);
-			tmp = NULL;
 			return;
 		}
 		if((*tree)->lPtr == NULL && (*tree)->rPtr != NULL) {
-			tmp = *tree;
 			*tree = (*tree)->rPtr;
-			free(tmp);
-			tmp = NULL;
 			return;
 		}
 		if((*tree)->lPtr == NULL && (*tree)->rPtr == NULL) {
-			free(*tree);
 			*tree = NULL;
 			return;
 		}
 	}
 	else {
-		if((*tree)->key > K)
+		if(strcmp((*tree)->key, K) > 0)
 			nodeDelete(&(*tree)->lPtr, K);
 		else
 			nodeDelete(&(*tree)->rPtr, K);
@@ -110,6 +115,7 @@ void nodeDelete(nodePtr *tree, char *K) {
 }
 
 void generateKey(char symbolName[64], int metaType) {
+	// TODO - udelat ze vseho lower case
 	// variable
 	if(metaType == 1) {
 		strcat(symbolName, "v");
@@ -119,3 +125,366 @@ void generateKey(char symbolName[64], int metaType) {
 		strcat(symbolName, "f");
 	}
 }
+
+
+
+int insert_variable(char *name, int type) {
+	nodePtr uzel;
+	char tmp[1000];
+
+	loadPtr Content_of_Insert2 = saveMalloc(sizeof(struct load));
+	//val tmptmp = malloc(sizeof(struct value));
+	strcpy(tmp, name);
+	generateKey(tmp,1);
+
+	uzel = nodeSearch(Strom, tmp);
+	if(uzel != NULL) {
+		fprintf(stderr, "Chyba: Promenna jiz existuje\n");
+		return FAIL;
+	}
+	else {
+		// TODO if value == NULL, pak se jedna o definici
+		// pokud se jedna o inicializaci, musi uz byt definovana
+		Content_of_Insert2->defined = 1;
+		Content_of_Insert2->type = type;
+		Content_of_Insert2->metaType = 1;
+		nodeInsert(&Strom, Content_of_Insert2, tmp);
+	}
+	return 0;
+}
+
+int insert_value(char *name, int type, void* pointer) {
+	nodePtr uzel;
+	char tmp[1000]; // TODO limit je 1000 znaku
+	strcpy(tmp, name);
+	generateKey(tmp,1);
+	uzel = nodeSearch(Strom, tmp);
+
+	if(uzel == NULL) {
+		fprintf(stderr, "Chyba: Promenna neexistuje\n");
+		return FAIL;
+	}
+	if(type == 1) {
+		uzel->symbol->value.i = *((int*)pointer);
+	}
+	else if(type == 2) {
+		uzel->symbol->value.d = *((double*)pointer);
+	}
+	else if(type == 3) {
+		//(struct String*)pointer;
+		uzel->symbol->value.s.data = ((char*)pointer);
+	}
+	else 
+		return FAIL;
+	return 0;
+}
+
+// vlozit param do funkci
+
+int insert_function(bool declared, char *name) {
+	nodePtr uzel;
+	char tmp[1000];
+	strcpy(tmp, name);
+	generateKey(tmp, 2);
+	uzel = nodeSearch(Strom, tmp);
+
+	if(declared == true) {
+		// pokud chceme deklarovat
+		if(uzel != NULL) {
+			// pokud jiz uzel ve strome je - chyba
+			fprintf(stderr, "Chyba: Promenna jiz existje\n");
+			return FAIL;
+		}
+		// else if(uzel->symbol->function.declared == 1) {
+		// 	fprintf(stderr, "Chyba: Funkce jiz byla deklarvana\n");
+		// 	return FAIL;
+		// }
+		else {
+			Content_of_Insert->metaType = 2;
+			Content_of_Insert->defined = false;
+			Content_of_Insert->function.declared = true;
+			Content_of_Insert->function.hasReturn = false;
+
+			nodeInsert(&Strom, Content_of_Insert, tmp);
+			return 0;			
+		}
+	}
+	else {
+		// definice
+		if(uzel == NULL) {
+			fprintf(stderr, "Chyba: Promenna neexistuje\n");
+			return FAIL;
+		}
+		else if(uzel->symbol->defined == 1) {
+			fprintf(stderr, "Chyba: Snaha o dvoji definici\n");
+			return FAIL;
+		}
+		if(uzel->symbol->function.declared == 1)
+			uzel->symbol->defined = 1;
+	}
+	return 0;
+}
+
+int insert_type_into_f(char *name, int type) {
+	nodePtr uzel;
+	char tmp[1000];
+	strcpy(tmp, name);
+	generateKey(tmp,2);
+	uzel = nodeSearch(Strom, tmp);	
+	uzel->symbol->type = type;
+	return 0;
+}
+
+int set_hasReturn(char *name) {
+	nodePtr uzel;
+	char tmp[1000];
+	strcpy(tmp, name);
+	generateKey(tmp,2);
+	uzel = nodeSearch(Strom, tmp);	
+	uzel->symbol->function.hasReturn = true;
+	return 0;
+}
+
+int insert_param(char *name, parStruct *par) {
+	nodePtr uzel;
+	char tmp[1000];
+	strcpy(tmp, name);
+	generateKey(tmp,2);
+	uzel = nodeSearch(Strom, tmp);	
+	
+	if(uzel->symbol->function.parameters == NULL) {
+		struct parameters *Par1 = saveMalloc(sizeof(*Par1));
+		(*Par1).type = (*par).type;
+		strcpy((*Par1).name, (*par).id);
+		uzel->symbol->function.parameters = &(*Par1);
+	}	
+	else {
+		param tmp = uzel->symbol->function.parameters;
+		printf("%s\n", tmp->name);
+		// &(*Par1) = uzel->symbol->function.parameters;
+		//printf("%s\n", (*Par1).name);
+		while(tmp->next != NULL) {		
+			tmp = tmp->next;
+		}
+		struct parameters *Par1 = saveMalloc(sizeof(*Par1));
+		(*Par1).type = (*par).type;
+		strcpy((*Par1).name, (*par).id);	
+		tmp->next = &(*Par1);		
+	}
+	return 0;
+}
+
+// int insert_variable_int(char *name, int hodnota) {
+// 	nodePtr uzel;
+// 	char tmp[1000];
+
+	
+// 	loadPtr Content_of_Insert2 = saveMalloc(sizeof(struct load));
+// 	//val tmptmp = malloc(sizeof(struct value));
+
+// 	strcpy(tmp, name);
+// 	generateKey(tmp,1);
+
+// 	uzel = nodeSearch(Strom, tmp);
+// 	if(uzel != NULL) {
+// 		fprintf(stderr, "Chyba: Promenna jiz existuje\n");
+// 		return FAIL;
+// 	}
+// 	else {
+// 		// TODO if value == NULL, pak se jedna o definici
+// 		// pokud se jedna o inicializaci, musi uz byt definovana
+// 		Content_of_Insert2->value.i = hodnota;
+// 		Content_of_Insert2->defined = 1;
+// 		Content_of_Insert2->type = 1;
+// 		Content_of_Insert2->metaType = 1;
+// 		Content_of_Insert2->function.declared = 1;
+// 		Content_of_Insert2->function.hasReturn = 0;
+// 		nodeInsert(&Strom, Content_of_Insert2, tmp);
+// 	}
+// 	return 0;
+// }
+
+// int insert_variable_double(char *name, double hodnota) {
+// 	nodePtr uzel;
+// 	char tmp[1000];
+
+// 	loadPtr Content_of_Insert2 = saveMalloc(sizeof(struct load));
+
+// 	strcpy(tmp, name);
+// 	generateKey(tmp,1);
+
+// 	uzel = nodeSearch(Strom, tmp);
+// 	if(uzel != NULL) {
+// 		fprintf(stderr, "Chyba: Promenna jiz existuje\n");
+// 		return FAIL;
+// 	}
+// 	else {
+// 		// TODO if value == NULL, pak se jedna o definici
+// 		// pokud se jedna o inicializaci, musi uz byt definovana
+// 		Content_of_Insert2->value.d = hodnota;
+// 		Content_of_Insert2->defined = 1;
+// 		Content_of_Insert2->type = 2;
+// 		Content_of_Insert2->metaType = 1;
+// 		Content_of_Insert2->function.declared = 1;
+// 		Content_of_Insert2->function.hasReturn = 0;
+// 		nodeInsert(&Strom, Content_of_Insert2, tmp);
+// 	}
+// 	return 0;	
+// }
+
+// int insert_variable_string(char *name, char* hodnota) {
+// 	nodePtr uzel;
+// 	char tmp[1000];
+
+// 	loadPtr Content_of_Insert2 = saveMalloc(sizeof(struct load));
+
+// 	strcpy(tmp, name);
+// 	generateKey(tmp,1);
+
+// 	uzel = nodeSearch(Strom, tmp);
+// 	if(uzel != NULL) {
+// 		fprintf(stderr, "Chyba: Promenna jiz existuje\n");
+// 		return FAIL;
+// 	}
+// 	else {
+// 		printf(">>>>>>1<<<<<<<%s\n", hodnota);
+// 		// TODO if value == NULL, pak se jedna o definici
+// 		// pokud se jedna o inicializaci, musi uz byt definovana
+// 		stringCpy(&Content_of_Insert2->value.s, hodnota);
+// 		printf("%s\n", hodnota);
+// 		printf(">>>>>>2<<<<<<<%s\n",Content_of_Insert2->value.s.data);
+// 		Content_of_Insert2->defined = 1;
+// 		Content_of_Insert2->type = 3;
+// 		Content_of_Insert2->metaType = 1;
+// 		Content_of_Insert2->function.declared = 1;
+// 		Content_of_Insert2->function.hasReturn = 0;
+// 		nodeInsert(&Strom, Content_of_Insert2, tmp);
+// 	}
+// 	return 0;	
+// }
+
+
+
+/*
+int insert_function(bool declared, int type, char *name, parStruct *par) {
+	nodePtr uzel;
+	char tmp[1000];
+
+	// pridame na konec jmena funkce znak f
+	// ve strome budou nazvy uz s f/v na konci
+	strcpy(tmp, name);
+	generateKey(tmp, 2);
+
+
+	uzel = nodeSearch(Strom, tmp);
+
+	if(uzel != NULL) {
+		// uzel uz ve strome existuje
+		// jedna se bud o promennou, nebo o funkci, ktera jiz byla dekalrovana nebo definovana
+		if(uzel->symbol->metaType == 1) {
+			// pokud se jedna o promennou, skonci automaticky s chybou
+			fprintf(stderr, "Chyba: Promenna jiz existuje\n");
+			return FAIL;
+		}
+		else {
+			// pokud se jedna o dvoji deklaraci nebo o definici
+			if(declared == 1) {
+				// jedna se o dvoji deklaraci - skonci s chybou
+				fprintf(stderr, "Chyba: Pokousite se podruhe deklarovat stejnou funkci\n");
+				return FAIL;
+			}
+			else if(uzel->symbol->defined == 1) {
+				fprintf(stderr, "Chyba: Pokousite se znovu definovat jiz definovanou funkci\n");
+				return FAIL;
+			}
+			else {
+				// pokud se jedna o definici
+				// aktualizuji se data ve strome a funkce uspesne konci
+				uzel->symbol->defined = 1;
+				return 0;
+			}
+		}
+	}
+	else {
+		// pokud uzel ve strome jete neni, znamena to, ze se jedna o funcki, ktera se ted muze byt teprve deklrovat, nebo primo i definovat
+		if(declared == 1) {
+			// pro deklaraci nove funkce
+			Content_of_Insert->type = type;
+			Content_of_Insert->metaType = 2;
+			Content_of_Insert->defined = false;
+			Content_of_Insert->function.declared = true;
+			Content_of_Insert->function.hasReturn = false;
+
+			struct parameters *Par1 = saveMalloc(sizeof(*Par1));
+			struct parameters *Par2 = saveMalloc(sizeof(*Par2));
+			struct parameters *Par3 = saveMalloc(sizeof(*Par3));
+
+			(*Par1).type = par[0].type;
+			strcpy((*Par1).name, par[0].id);
+			(*Par2).type = par[1].type;
+			strcpy((*Par2).name, par[1].id);
+			(*Par3).type = par[2].type;
+			strcpy((*Par3).name, par[2].id);
+
+			(*Par1).next = &(*Par2);
+			(*Par2).next = &(*Par3);
+
+			Content_of_Insert->function.parameters = &(*Par1); // ukazatel na prvni rpvek seznamu
+
+			nodeInsert(&Strom, Content_of_Insert, tmp);
+			return 0;
+		}
+		else {
+			Content_of_Insert->type = type;
+			Content_of_Insert->metaType = 2;
+			Content_of_Insert->defined = true;
+			Content_of_Insert->function.declared = true;
+			Content_of_Insert->function.hasReturn = false;
+
+			struct parameters *Par1 = saveMalloc(sizeof(*Par1));
+			struct parameters *Par2 = saveMalloc(sizeof(*Par2));
+			struct parameters *Par3 = saveMalloc(sizeof(*Par3));
+
+			(*Par1).type = par[0].type;
+			strcpy((*Par1).name, par[0].id);
+			(*Par2).type = par[1].type;
+			strcpy((*Par2).name, par[1].id);
+			(*Par3).type = par[2].type;
+			strcpy((*Par3).name, par[2].id);
+
+			(*Par1).next = &(*Par2);
+			(*Par2).next = &(*Par3);
+
+			Content_of_Insert->function.parameters = &(*Par1); // ukazatel na prvni rpvek seznamu
+
+			nodeInsert(&Strom, Content_of_Insert, tmp);
+			return 0;
+		}
+	}
+
+
+	// // pokud je declared 0, jedna se o dekalraci
+	// // jinak je to definice
+	// if(declared == 1) {
+	// 	//printf("%d\n", uzel->key->function->declared);
+	// 	// poku uz byla funcke jednou delarovana
+	// 	// zkontrolujeme, jestli uz byla deklarovana, pokud byla, tak ukoncit s chybou dvoji deklarace
+	// }
+	// else {
+	// 	// TODO musime zkontrolovat, jestli uz nahodou nebyla definovana jednou
+	// }
+
+
+
+
+
+
+	// printf("-------------  %s\n", Strom->key);
+	// printf("%d\n", declared);
+	// printf("%d\n", type);
+	// printf("%s\n", name);
+	// printf("%d\n", par[1].type);
+	// printf("%s\n", par[1].id);
+	return 0;
+}
+*/
--- Source/symtable.h
+++ Source/symtable.h
@@ -1,24 +1,29 @@
 /*
  * IFJ 2017 project
- *	File			symtable.h
- *	Description		header file for binary search tree
- *	Author			Martin Studeny (xstude23)
+ *  File            symtable.h
+ *  Description     header file for binary search tree
+ *  Author          Martin Studeny (xstude23)
  */
-#ifndef SYMTABLEH 	// obaleni pro vicenasobny preklad
+#ifndef SYMTABLEH   // obaleni pro vicenasobny preklad
 #define SYMTABLEH 
 
 #include "includes.h"
 
 typedef struct parameters *param;
 
-typedef struct {
+typedef struct defParam {
+    char id[64];
+    int type;
+} parStruct;
+
+typedef struct value {
     int i;
     double d;
-    char *s;
+    String s;
 } val;
 
 struct parameters{
-    char *name;
+    char name[1000];
     int type; // see description in struct load
     param next;
 };
@@ -26,12 +31,12 @@ struct parameters{
 typedef struct {
     param parameters; // linear list of function parametes
     //nodePtr functTable;  // function local symbol table
-    bool hasReturn = false;
+    bool hasReturn;
     bool declared;
 } functInfo;
 
 typedef struct load {
-    bool defined;
+    bool defined; // TODO: -automatiky na 1
     int type; // 1 => int; 2 => double; 3 => string; 
     int metaType; // 1 => variable; 2 => function;
     val value;
@@ -39,17 +44,29 @@ typedef struct load {
 } *loadPtr;
 
 typedef struct node {
-    char *key;
+    char key[64];
     struct node *lPtr;
     struct node *rPtr;
     loadPtr symbol;
 } *nodePtr;
 
 void treeInit(nodePtr*);
-nodePtr nodeInsert(nodePtr*, loadPtr, char*);
+void nodeInsert(nodePtr*, loadPtr, char*);
 void nodeDelete(nodePtr*, char*);
 void treeDispose (nodePtr*);
-nodePtr nodeSearch(nodePtr, char*, loadPtr*);
+nodePtr nodeSearch(nodePtr, char*);
 void generateKey(char *symbolName, int metaType);
+int insert_variable(char *name, int type);
+int insert_value(char *name, int type, void* pointer);
+int insert_function(bool declared, char *name);
+int insert_type_into_f(char *name, int type);
+int set_hasReturn(char *name);
+int insert_param(char *name, parStruct *par);
+
+
+int insert_variable_int(char *name, int hodnota);
+int insert_variable_double(char *name, double hodnota);
+int insert_variable_string(char *name, char* hodnota);
+
 
 #endif
